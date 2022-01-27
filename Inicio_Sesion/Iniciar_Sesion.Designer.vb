92140d4054ffe3ffcc6910943e662bbbd2405f06
daros <daros@DESKTOP-9JJ2A4E>
2022-01-27 14:46:24 -0600

    Agregar archivos de proyecto.

---
+++ Inicio_Sesion/Iniciar_Sesion.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Iniciar_Sesion
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelTitulo = New System.Windows.Forms.Label()
+        Me.LabelFacultad = New System.Windows.Forms.Label()
+        Me.PanelBarraTitutlo = New System.Windows.Forms.Panel()
+        Me.ButtonSalir = New System.Windows.Forms.Button()
+        Me.ButtonMinimizar = New System.Windows.Forms.Button()
+        Me.PanelBarraTitutlo.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LabelTitulo
+        '
+        Me.LabelTitulo.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.LabelTitulo.AutoSize = True
+        Me.LabelTitulo.Font = New System.Drawing.Font("Niagara Engraved", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelTitulo.ForeColor = System.Drawing.Color.White
+        Me.LabelTitulo.Location = New System.Drawing.Point(69, 55)
+        Me.LabelTitulo.Name = "LabelTitulo"
+        Me.LabelTitulo.Size = New System.Drawing.Size(120, 34)
+        Me.LabelTitulo.TabIndex = 0
+        Me.LabelTitulo.Text = "Iniciar Sesión"
+        '
+        'LabelFacultad
+        '
+        Me.LabelFacultad.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.LabelFacultad.AutoSize = True
+        Me.LabelFacultad.Font = New System.Drawing.Font("Niagara Engraved", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelFacultad.ForeColor = System.Drawing.Color.White
+        Me.LabelFacultad.Location = New System.Drawing.Point(56, 375)
+        Me.LabelFacultad.Name = "LabelFacultad"
+        Me.LabelFacultad.Size = New System.Drawing.Size(153, 26)
+        Me.LabelFacultad.TabIndex = 1
+        Me.LabelFacultad.Text = "Facultad de Informática"
+        '
+        'PanelBarraTitutlo
+        '
+        Me.PanelBarraTitutlo.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(79, Byte), Integer))
+        Me.PanelBarraTitutlo.Controls.Add(Me.ButtonMinimizar)
+        Me.PanelBarraTitutlo.Controls.Add(Me.ButtonSalir)
+        Me.PanelBarraTitutlo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelBarraTitutlo.Location = New System.Drawing.Point(0, 0)
+        Me.PanelBarraTitutlo.Name = "PanelBarraTitutlo"
+        Me.PanelBarraTitutlo.Size = New System.Drawing.Size(257, 32)
+        Me.PanelBarraTitutlo.TabIndex = 2
+        '
+        'ButtonSalir
+        '
+        Me.ButtonSalir.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ButtonSalir.FlatAppearance.BorderSize = 0
+        Me.ButtonSalir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkRed
+        Me.ButtonSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red
+        Me.ButtonSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonSalir.ForeColor = System.Drawing.SystemColors.MenuBar
+        Me.ButtonSalir.Location = New System.Drawing.Point(225, 0)
+        Me.ButtonSalir.Name = "ButtonSalir"
+        Me.ButtonSalir.Size = New System.Drawing.Size(32, 32)
+        Me.ButtonSalir.TabIndex = 3
+        Me.ButtonSalir.Text = "X"
+        Me.ButtonSalir.UseVisualStyleBackColor = True
+        '
+        'ButtonMinimizar
+        '
+        Me.ButtonMinimizar.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ButtonMinimizar.FlatAppearance.BorderSize = 0
+        Me.ButtonMinimizar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver
+        Me.ButtonMinimizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.ButtonMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonMinimizar.ForeColor = System.Drawing.SystemColors.MenuBar
+        Me.ButtonMinimizar.Location = New System.Drawing.Point(193, 0)
+        Me.ButtonMinimizar.Name = "ButtonMinimizar"
+        Me.ButtonMinimizar.Size = New System.Drawing.Size(32, 32)
+        Me.ButtonMinimizar.TabIndex = 4
+        Me.ButtonMinimizar.Text = "_"
+        Me.ButtonMinimizar.UseVisualStyleBackColor = True
+        '
+        'Iniciar_Sesion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(47, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(74, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(257, 410)
+        Me.Controls.Add(Me.PanelBarraTitutlo)
+        Me.Controls.Add(Me.LabelFacultad)
+        Me.Controls.Add(Me.LabelTitulo)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Iniciar_Sesion"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Iniciar_Sesion"
+        Me.PanelBarraTitutlo.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LabelTitulo As Label
+    Friend WithEvents LabelFacultad As Label
+    Friend WithEvents PanelBarraTitutlo As Panel
+    Friend WithEvents ButtonSalir As Button
+    Friend WithEvents ButtonMinimizar As Button
+End Class
